90fd87e6a0f53ac0f8cfe547849844ce182cb428
Alexandra Chastain <chastain.alexandra@gmail.com>
2022-12-23 19:39:14 -0500

    day16

--- day13.py
+++ day13.py
@@ -4,6 +4,8 @@
 # Your list consists of pairs of packets; pairs are separated by a blank line. You need to identify how many pairs of packets are in the right order.
 
 from functools import cmp_to_key
+import json
+from copy import deepcopy
 
 def compare(a, b):
     if isinstance(a, list) and isinstance(b, int):
@@ -52,29 +54,96 @@ def part01(data):
 # [[6]]
 # Using the same rules as before, organize all packets - the ones in your list of received packets as well as the two divider packets - into the correct order.
 
-def part02(data): 
-    lists = list(map(eval, data)) #error here!
-    lists.append([[2]])
-    lists.append([[6]])
-    lists = sorted(lists, key=cmp_to_key(compare), reverse=True)
+def comparePart2(a,b):
+    def compare(a, b):
+        for i in range(len(a)):
+            if i >= len(b):
+                return False, None
+
+            atype = type(a[i])
+            btype = type(b[i])
+            # print(f"Looking at {a[i]} and {b[i]}. {atype} vs {btype}")
+
+            ## First pass: Upgrade singleton lists
+            if atype == int and btype == list:
+                a = deepcopy(a)
+                a[i] = [a[i]]
+                atype = type(a[i])
+            elif atype == list and btype == int:
+                b = deepcopy(b)
+                b[i] = [b[i]]
+                btype = type(b[i])
+
+            if atype == int and btype == int:
+                #  If the left integer is higher than the right integer,
+                #  the inputs are not in the right order.
+                if a[i] < b[i]:
+                    # print(f"Returning true because left int is smaller {a[i]} < {b[i]}")
+                    return True, True
+                if a[i] > b[i]:
+                    # print(f"Returning false because left int is higher {a[i]} > {b[i]}")
+                    return False, None
+            elif atype == list and btype == list:
+                compare_result, compare_override = compare(a[i], b[i])
+                if not compare_result:
+                    # print("Returning false because compare returned false")
+                    return False, None
+                if compare_override:
+                    # print("Returning true because compare returned true")
+                    return True, True
+            else:
+                raise Exception(f"Unknown types {atype} {btype}")
+
+    # We compared everything. We're either in a "left ran out" state or a
+    # "everything is equal" state, which is somewhat indeterminate.
+    return True, len(a) < len(b)
 
 
-    for i, li in enumerate(lists):
-        if li == [[2]]:
-            a = i + 1
-        if li == [[6]]:
-            b = i + 1
 
-    print(a * b)
+def comparePart2(a,b):
+    resultsComp, overrideComp = comparePart2(a,b)
+    if resultsComp and overrideComp: 
+        return -1
+    elif not resultsComp: 
+        return 1
+    else: 
+        return 0
 
+def getFormattedData():
+     with open("day13.txt") as f:
+        data = f.read().strip()
+        blocks = data.split("\n\n")
+        return [parse_block(block) for block in blocks]  
+
+def parse_block(block: str):
+    b1, b2 = block.split("\n")
+    return json.loads(b1), json.loads(b2)   
+
+def part02(): 
+    data = getFormattedData()
+    pairs = []
+
+    for(left, right) in data: 
+        pairs.append(left)
+        pairs.append(right)
     
+    pairs.append([[2]])
+    pairs.append([[6]])
+
+    pairs.sort(key=cmp_to_key(comparePart2))
+    total = 1
+    for i, p in enumerate(pairs,1): 
+        if p == [[6]] or p == [[2]]:
+            total *= i
+    
+    print(total)
 
 if __name__ == "__main__":
    with open("day13.txt") as fin:
     lines = fin.read().strip().split("\n\n")
 
    part01(lines)
-   part02(lines)
+   part02()
 
 # answers 
 # Part 1 - 6428
--- day16.py
+++ day16.py
@@ -1,80 +1,311 @@
-from __future__ import annotations
-
-import argparse
-import collections
-import itertools
-import os.path
 import re
+from dataclasses import dataclass
+from collections import defaultdict, deque
+from typing import NamedTuple
+
+PARSER = re.compile(
+    r"Valve (\w+) has flow rate=(\d+); tunnel(?:s)? lead(?:s)? to valve(?:s)? (.*)"
+)
+
+MAX_MINUTES = 30
+
+@dataclass()
+class Valve:
+    name: str
+    flow_rate: int
 
 
-RATE = re.compile(r'rate=(\d+);')
-VALVES = re.compile(r'to valves? (.*)$')
-
-INPUT_TXT = os.path.join(os.path.dirname(__file__), 'puzzleFile.txt')
-
-
-def compute(s: str) -> int:
-    edges = {}
-    rates = {}
-
-    for line in s.splitlines():
-        _, name, *_ = line.split()
-        match_valves = VALVES.search(line)
-        assert match_valves is not None
-        targets = match_valves[1].split(', ')
-        match = RATE.search(line)
-        assert match is not None
-        edges[name] = targets
-        rates[name] = int(match[1])
-
-    weights = {}
-    positive_rates = frozenset(k for k, v in rates.items() if v)
-    meaningful_edges = ['AA', *positive_rates]
-    for a, b in itertools.combinations(meaningful_edges, r=2):
-        todo_bfs: collections.deque[tuple[str, ...]]
-        todo_bfs = collections.deque([(a,)])
-        while todo_bfs:
-            path = todo_bfs.popleft()
-            if path[-1] == b:
-                break
-            else:
-                todo_bfs.extend((*path, n) for n in edges[path[-1]])
-        weights[(a, b)] = len(path)
-        weights[(b, a)] = len(path)
-
-    # time to total
-    best = -1
-    todo: list[tuple[int, int, tuple[str, ...], frozenset[str]]]
-    todo = [(0, 0, ('AA',), positive_rates)]
-    while todo:
-        score, time, route, possible = todo.pop()
-        best = max(best, score)
-
-        for p in possible:
-            needed_time = time + weights[(route[-1], p)]
-            if needed_time < 30:
-                todo.append((
-                    score + (30 - needed_time) * rates[p],
-                    needed_time,
-                    route + (p,),
-                    possible - {p},
-                ))
-
-    return best
+State = NamedTuple(
+    "State",
+    [
+        ("loc", str),
+        ("open_valves", frozenset),
+        ("pressure_released", int),
+        ("minutes", int),
+        ("last_loc", str),
+    ],
+)
+StateNoLastLoc = NamedTuple(
+    "StateNoLastLoc",
+    [
+        ("loc", str),
+        ("open_valves", frozenset),
+        ("pressure_released", int),
+        ("minutes", int),
+    ],
+)
+StateNoPres = NamedTuple(
+    "StateNoPres",
+    [
+        ("loc", str),
+        ("open_valves", frozenset),
+        ("minutes", int),
+    ],
+)
 
 
+def get_state_no_last_loc(s):
+    return StateNoLastLoc(s.loc, s.open_valves, s.pressure_released, s.minutes)
 
 
-def main() -> int:
-    parser = argparse.ArgumentParser()
-    parser.add_argument('data_file', nargs='?', default=INPUT_TXT)
-    args = parser.parse_args()
-
-    with open(args.data_file) as f:
-        print(compute(f.read()))
-
-    return 0
+def reverse_me_and_ele(snll):
+    locs = snll.loc.split(",")
+    locs = f"{locs[1]},{locs[0]}"
+    return StateNoLastLoc(locs, snll.open_valves, snll.pressure_released, snll.minutes)
 
 
-if __name__ == '__main__':
-    raise SystemExit(main())
+def get_state_no_press(s):
+    # return StateNoPres(s.loc, s.open_valves, s.minutes)
+    # return StateNoPres(s.loc, frozenset(), s.minutes)
+    return StateNoPres("XYZ", s.open_valves, s.minutes)
+
+
+def parse(filename):
+    valves = {}
+    vmap = defaultdict(list)
+
+    with open(filename) as file:
+        for line in file.readlines():
+            line = line.strip()
+
+            (valve, flow, tunnels) = re.search(PARSER, line).groups()
+            flow = int(flow)
+            valves[valve] = Valve(valve, flow)
+
+            tunnels = tunnels.split(", ")
+            for dest_tunnel in tunnels:
+                vmap[valve].append(dest_tunnel)
+
+    return valves, vmap
+
+
+def init_state():
+    return State("AA", frozenset(), 0, 0, "")
+
+
+def init_state_ele():
+    return State("AA,AA", frozenset(), 0, 0, ",")
+
+
+def get_neighbors(s, valves, vmap):
+    if s.minutes >= MAX_MINUTES:
+        return []
+
+    r = []
+    last_loc = s.loc
+
+    pressure_delta = 0
+    for ov in s.open_valves:
+        pressure_delta += valves[ov].flow_rate
+    new_pressure = s.pressure_released + pressure_delta
+
+    ## Moving to another valve
+    for dest in vmap[s.loc]:
+        if dest == s.last_loc:
+            continue
+        r.append(State(dest, s.open_valves, new_pressure, s.minutes + 1, last_loc))
+
+    ## Opening a valve
+    if s.loc not in s.open_valves and valves[s.loc].flow_rate > 0:
+        r.append(
+            State(s.loc, s.open_valves | {s.loc}, new_pressure, s.minutes + 1, last_loc)
+        )
+
+    return r
+
+
+def get_neighbors_ele(s, valves, vmap):
+    if s.minutes >= MAX_MINUTES:
+        return []
+
+    r = []
+
+    last_loc_me, last_loc_ele = s.last_loc.split(",")
+    loc_me, loc_ele = s.loc.split(",")
+
+    pressure_delta = 0
+    for ov in s.open_valves:
+        pressure_delta += valves[ov].flow_rate
+    new_pressure = s.pressure_released + pressure_delta
+
+    ## (ME+Ele) Moving to another valve
+    my_acts = []
+    ele_acts = []
+    for dest_me in vmap[loc_me]:
+        if dest_me == last_loc_me:
+            continue
+        my_acts.append(["move", dest_me])
+
+    for dest_ele in vmap[loc_ele]:
+        if dest_ele == last_loc_ele:
+            continue
+        ele_acts.append(["move", dest_ele])
+
+    ## Opening a valve
+    if loc_me not in s.open_valves and valves[loc_me].flow_rate > 0:
+        my_acts.append(["open", loc_me])
+    if loc_ele not in s.open_valves and valves[loc_ele].flow_rate > 0:
+        ele_acts.append(["open", loc_ele])
+
+    for my_act in my_acts:
+        for ele_act in ele_acts:
+
+            new_open_valves = s.open_valves
+            if my_act[0] == "open":
+                new_open_valves = new_open_valves | {my_act[1]}
+            if ele_act[0] == "open":
+                new_open_valves = new_open_valves | {ele_act[1]}
+
+            new_loc_me = loc_me
+            new_loc_ele = loc_ele
+            if my_act[0] == "move":
+                new_loc_me = my_act[1]
+            if ele_act[0] == "move":
+                new_loc_ele = ele_act[1]
+
+            if new_loc_me == new_loc_ele:
+                continue
+            r.append(
+                State(
+                    f"{new_loc_me},{new_loc_ele}",
+                    new_open_valves,
+                    new_pressure,
+                    s.minutes + 1,
+                    s.loc,
+                )
+            )
+
+    return r
+
+
+
+
+
+
+
+# The sensors have led you to the origin of the distress signal: yet another handheld device, just like the one the Elves gave you. However, you don't see any Elves around; instead, the device is surrounded by elephants! They must have gotten lost in these tunnels, 
+# and one of the elephants apparently figured out how to turn on the distress signal.
+# The ground rumbles again, much stronger this time. What kind of cave is this, exactly? 
+# You scan the cave with your handheld device; it reports mostly igneous rock, some ash,
+#  pockets of pressurized gas, magma... this isn't just a cave, it's a volcano!
+# You need to get the elephants out of here, quickly. 
+# Your device estimates that you have 30 minutes before the volcano erupts, so you don't have time to go back out the way you came in.
+# You scan the cave for other options and discover a 
+# network of pipes and pressure-release valves. You aren't sure how such a system got into a volcano, but you don't have time to complain; your device produces a report (your puzzle input) of each valve's flow rate if it were opened (in pressure per minute) and the tunnels you could use to move between the valves.
+# There's even a valve in the room you and the elephants are currently standing 
+# in labeled AA. You estimate it will take you one minute to open a single valve 
+# and one minute to follow any tunnel from one valve to another. What is the most pressure you could release?
+
+def part01(valves, map):
+    q = deque([init_state()])
+    seen = set()
+    i = 0
+
+    final_states = []
+    max_press_for = defaultdict(int)
+        
+    while q:
+        s = q.popleft()  # BFS
+        # s = q.pop()  # DFS
+
+        ## Skip if we've seen this state before
+        snll = get_state_no_last_loc(s)
+        if snll in seen:
+            continue
+        seen.add(snll)
+
+        ## Skip if we've seen this state before, but with more pressure
+        snp = get_state_no_press(s)
+        if s.pressure_released < max_press_for[snp]:
+            continue
+        max_press_for[snp] = s.pressure_released
+
+        i += 1
+        if i % 500000 == 0:
+            maxp = 0
+            if len(final_states) > 0:
+                maxp = max(s.pressure_released for s in final_states)
+            print(
+                f"Seen {i} states | {len(q)} in queue | {len(seen)} seen | s.minutes={s.minutes} | current max {maxp}"
+            )
+
+        # print(s)
+        for n in get_neighbors(s, valves, map):
+            # print("--> ", n)
+            q.append(n)
+
+        if s.minutes >= MAX_MINUTES:
+            final_states.append(s)
+
+    m = max(final_states, key=lambda s: s.pressure_released)
+    # print(m)
+    return m.pressure_released
+
+
+# You're worried that even with an optimal approach, the pressure released won't be enough. 
+# What if you got one of the elephants to help you?
+# It would take you 4 minutes to teach an elephant how to open the right valves
+#  in the right order, leaving you with only 26 minutes to actually execute your plan. 
+#  Would having two of you working together be better, 
+#  even if it means having less time? (Assume that you teach the elephant before opening any valves yourself, giving you both the same full 26 minutes.)
+
+def part02(valves, vmap):
+    q = deque([init_state_ele()])
+    seen = set()
+    i = 0
+
+    final_states = []
+    max_press_for = defaultdict(int)
+
+    while q:
+        s = q.popleft()  # BFS
+        # s = q.pop()  # DFS
+
+        ## Skip if we've seen this state before
+        snll = get_state_no_last_loc(s)
+        if snll in seen:
+            continue
+        seen.add(snll)
+        seen.add(reverse_me_and_ele(snll))
+
+        ## Skip if we've seen this state before, but with more pressure
+        snp = get_state_no_press(s)
+        if s.pressure_released < max_press_for[snp]:
+            continue
+        max_press_for[snp] = s.pressure_released
+
+        i += 1
+        if i % 500000 == 0:
+            maxp = 0
+            if len(final_states) > 0:
+                maxp = max(s.pressure_released for s in final_states)
+            print(
+                f"Seen {i} states | {len(q)} in queue | {len(seen)} seen | s.minutes={s.minutes} | current max {maxp}"
+            )
+
+        # print(s)
+        for n in get_neighbors_ele(s, valves, vmap):
+            # print("--> ", n)
+            q.append(n)
+
+        if s.minutes >= MAX_MINUTES:
+            final_states.append(s)
+
+        if len(q) > 1000000:
+            q = deque(
+                sorted(q, key=lambda s: s.pressure_released, reverse=True)[:500000]
+            )
+
+    m = max(final_states, key=lambda s: s.pressure_released)
+    # print("Final state count:", i)
+    # print(m)
+    return m.pressure_released
+
+if __name__ == "__main__":
+    valves, vmap = parse("day16.txt")
+    print(part01(valves, vmap))
+    print(part02(valves, vmap))
+
+# answers 
+# Part 1 - 1845
+# Part 2 - 2286
