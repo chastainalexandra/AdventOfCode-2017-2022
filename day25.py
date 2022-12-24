6013b4a1f7f824b840ca019a98cfc8600b2eddef
Alexandra Chastain <chastain.alexandra@gmail.com>
2022-12-23 23:19:06 -0500

    shells for day 24 and day 25

--- day25.py
+++ day25.py
@@ -1,3 +1,20 @@
+
+
+
+
+def part01(lines):
+    print("part 1 - ")
+
+def part02(lines):
+    print("part 2 - ")
+
+if __name__ == "__main__":
+   with open("day24.txt") as fin:
+    lines = fin.read().strip().split("\n")
+   part01(lines)
+   part02(lines)
+
+   
 # answers 
 # Part 1 - 
 # Part 2 - 
