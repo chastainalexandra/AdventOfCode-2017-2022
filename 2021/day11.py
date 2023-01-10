a595fc3737d2ddec61733cec6a11ece88f2eef5b
Alexandra Chastain <chastain.alexandra@gmail.com>
2023-01-10 15:31:57 -0500

    Start to day 11 from 2021

--- 2021/day11.py
+++ 2021/day11.py
@@ -1,10 +1,25 @@
+import numpy as np
+from itertools import product # used for Functions creating iterators for efficient looping
+
+# After 100 steps, there have been a total of 1656 flashes.
+#Given the starting energy levels of the dumbo octopuses in your cavern, simulate 100 steps. How many total flashes are there after 100 steps?
+
 def part01(data):
+    answer = 0
+    dataLength = len(data)
+    octopuses = data
+
+    for steps in range(100):
+        flashed = np.zeros((dataLength, dataLength), dtype=bool)
+
+
+    print(answer)
 
 def part02(data):
-
+  print(data)
 if __name__ == "__main__":
    with open("day11.txt") as fin:
-    input = fin.read().strip().split(",")
-    data = [int(i) for i in input]
+    input = fin.read().strip()
+    data = np.array([[int(d) for d in list(i)] for i in input.split("\n")], dtype=int)
 part01(data) #Answer  
 part02(data) #Answer 
